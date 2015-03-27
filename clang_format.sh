11815d27208dc212637409184067f1f51bbfe0f0
Robin <me@invokr.org>
2015-03-27 13:51:23 +0000

    Added wrapper script to clang-format

---
+++ clang_format.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Copyright 2015 Robin Dietrich
+# Licensed under the AGPLv3
+#
+# Wrapper script around clang-format
+#
+# Usage:
+# ./clang_format.sh (style) (lines) (file)
+#
+# Example:
+# ./clang_format.sh Google 1:9999999 test.cpp
+# ./clang_format.sh "{key: value, ...}" 1:9999999 test.cpp
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+LINK="$DIR/clang_format"
+
+# Try to link clang-format to ./
+# We don't want to run the find step every time the script runs
+if [ ! -e ${LINK} ]; then
+    # Possible locations of clang-format
+    cfpath=(
+        "/usr/bin/clang-format"
+        "/usr/local/bin/clang-format"
+        "/usr/local/llvm34/bin/clang-format"
+        "/usr/local/llvm35/bin/clang-format"
+    )
+
+    # Try to find binary
+    binary=""
+
+    for path in "${cfpath[@]}"
+    do
+        if [ -e ${path} ]; then
+            binary=${path}
+        fi
+    done
+
+    if [ binary == "" ]; then
+        exit 0
+    else
+        ln -s ${binary} ${LINK}
+    fi
+fi
+
+# If we have clang-format, execute it
+if [ -e ${LINK} ]; then
+    echo "${LINK} -style=$1 -lines=$2 $3"
+    ${LINK} -style=$1 -lines=$2 $3
+fi
