6175dc6233f4b3a9973a58ae721d9f5639dc94bb
Robin <me@invokr.org>
2015-04-15 19:00:14 +0000

    Removed unused installer functions

--- install.js
+++ install.js
@@ -1,8 +1,8 @@
 define(function(require, exports, module) {
 
 module.exports = function(session, options){
-    session.introduction = require("text!./intro.html");
-    session.preInstallScript = require("text!./check-deps.sh");
+    //session.introduction = require("text!./intro.html");
+    //session.preInstallScript = require("text!./check-deps.sh");
 
     // libclang pkgs
     var pkgs = ['llvm-3.5', 'llvm-3.5-dev', 'lvm-3.5-runtime', 'libclang-3.5-dev', 'libclang1-3.5'];
